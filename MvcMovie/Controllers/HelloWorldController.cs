8a5876fc24b7f1061953b47b1679a1a833e6a708
Pedro Tauzen <pedrotauzen@gmail.com>
2024-01-13 20:32:21 +0000

    UptadesWelcome function

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
 
-namespace MvcMovie.Controllers
+namespace MvcMovie.Controllers;
+
+public class HelloWorldController : Controller
 {
-    public class HelloWorldController : Controller
+    // 
+    // GET: /HelloWorld/
+    public string Index()
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        return "This is my default action..";
     }
+    //
+    // GET: /HelloWorld/Welcome/
+    public string Welcome(string name, int numTimes = 1)
+    {
+        return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+    }
+
 }
+
